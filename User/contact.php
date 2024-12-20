6dccc2b450dce3e58026e47bcf373f9700fef76c
vtak1908 <vukiet252@gmail.com>
2024-12-20 11:53:34 +0700

    Update contact.php

--- User/contact.php
+++ User/contact.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['user'])) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Vegefoods</title>
+    <title>VegeFoods</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
